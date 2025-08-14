e773fc8bc5ef381cdc4f218672ecabbfa07b572a
aishwarya shinde <152557964+aishwaryagorakh@users.noreply.github.com>
2025-08-14 11:24:20 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',        // Ensures correct asset paths
+  base: './',        // Use relative paths for JS/CSS
   build: {
-    outDir: 'dist', // Explicit output directory
+    outDir: 'dist',  // Explicit output directory
   },
 })
