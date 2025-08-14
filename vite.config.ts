8e2137f7c47ab8d6b0ecb400fb157c4c5e1cd35c
aishwarya shinde <152557964+aishwaryagorakh@users.noreply.github.com>
2025-08-14 13:01:58 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',        // Use relative paths for JS/CSS
+//   build: {
+//     outDir: 'dist',  // Explicit output directory
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: './',        // Use relative paths for JS/CSS
-  build: {
-    outDir: 'dist',  // Explicit output directory
-  },
-})
+  
+  server: {
+    proxy: mode === 'development' ? {
+      '/api': 'http://localhost:9000'
+    } : undefined,
+  }
+}));
